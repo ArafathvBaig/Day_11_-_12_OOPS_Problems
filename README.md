242d57001ff2dbbf270b8a36af0d3a887aafdd58
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-04 19:17:26 +0530

    Added: .gitignore and Welcome Files to Master

---
+++ README.md
@@ -0,0 +1,1 @@
+ Day_11_-_12_OOPS_Problems
---
+++ src/com/Day_11_12_OOPS_Problems/WelcomeProgram.java
@@ -0,0 +1,9 @@
+package com.Day_11_12_OOPS_Problems;
+
+public class WelcomeProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To Object Oriented Practice Problems");
+	}
+}
