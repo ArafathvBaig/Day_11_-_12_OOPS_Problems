2beeae89f4332acb7c3a8e9ce3bb78b345b3eac0
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-05 13:32:24 +0530

    Added: Program File to Branch-2

---
+++ src/com/Day_11_12_OOPS_Problems/AccountTest.java
@@ -0,0 +1,31 @@
+package com.Day_11_12_OOPS_Problems;
+
+import java.util.Scanner;
+
+public class AccountTest 
+{
+	int balance = 5000;
+	int withDraw;
+	Scanner sc = new Scanner(System.in);
+	public void debit()
+	{
+		System.out.println("Enter The Amount To WithDraw : ");
+		withDraw = sc.nextInt();
+		if(withDraw<=balance)
+		{
+			System.out.println("Amount Debited From Main Balance : "+withDraw);
+			balance = balance-withDraw;
+			System.out.println("The Balance is : "+balance);
+		}
+		else
+		{
+			System.out.println("WithDraw Amount Exceeded The Main Balance");
+			System.out.println("The Balance is : "+balance);
+		}
+	}
+	public static void main(String[] args) 
+	{
+		AccountTest ac = new AccountTest();
+		ac.debit();
+	}
+}
